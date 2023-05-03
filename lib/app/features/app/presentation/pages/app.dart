cb7a11be376d65f0f667e2301ac50d6ea06552ab
Onyedikareal <frankonyeka25@gmail.com>
2023-05-03 23:10:28 +0100

    update: added theme data
    using google font package
    added light and dark theme setup

--- lib/app/features/app/presentation/pages/app.dart
+++ lib/app/features/app/presentation/pages/app.dart
@@ -2,6 +2,7 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:get/get_navigation/src/routes/transitions_type.dart';
+import 'package:mobmart/app/theme/theme.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
@@ -10,6 +11,8 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return  GetMaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: MobMartAppTheme.light(),
+      darkTheme: MobMartAppTheme.dark(),
       supportedLocales: context.supportedLocales,
       localizationsDelegates: context.localizationDelegates,
       defaultTransition: Transition.fade,
